374b07641277bb6efea17fd40c07f8a43808ec76
vaduwok-lgtm <vaduwok@gmail.com>
2026-07-15 11:30:57 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inventory-v5';
+const CACHE_NAME = 'inventory-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
